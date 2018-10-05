c78e6d559ae0756c785c161099b068a6c45af2f6
Nayshlok <borlandavid@gmail.com>
2018-10-05 06:26:47 -0600

    Hello world application
    Able to print hello world on desktop app

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import { baseDir } from './constants';
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600});
+  win.loadFile(baseDir + 'index.html');
+
+  win.on('closed', () => {
+    win = null;
+  })
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
